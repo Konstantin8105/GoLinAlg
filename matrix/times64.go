a7fbb1a565fb7a79c70155bf65951ecff6887c46
Konstantin8105 <konstantin8105@gmail.com>
2017-07-19 16:51:57 +0300

    pre

--- matrix/times64.go
+++ matrix/times64.go
@@ -1,16 +1,15 @@
 package matrix
 
 import (
+	"fmt"
 	"runtime"
 	"sync"
 )
 
 const (
 	sizeof  = 8.
-	memory  = 3000000.
-	floatL3 = int(memory / sizeof)
-	floatL2 = floatL3 / 10
-	floatL1 = floatL2 / 20
+	memory  = 3000000. / sizeof
+	floatL1 = memory / 200
 )
 
 // matrix a with size [m,n]
@@ -19,7 +18,16 @@ const (
 func timesAlgorithm(a, b, c *[][]float64, m, n, h int) {
 	sizeSummaryFloats := m*n + n*h + m*h
 	if sizeSummaryFloats < floatL1 {
-		timesAlgorithmSimple(a, b, c, m, n, h)
+		var sum float64
+		for j := 0; j < h; j++ {
+			for i := 0; i < m; i++ {
+				sum = 0
+				for k := 0; k < n; k++ {
+					sum += (*a)[i][k] * (*b)[k][j]
+				}
+				(*c)[i][j] = sum
+			}
+		}
 		return
 	}
 	// Found amount allowable parallelism
@@ -27,32 +35,73 @@ func timesAlgorithm(a, b, c *[][]float64, m, n, h int) {
 	if threads > runtime.NumCPU() {
 		threads = runtime.NumCPU()
 	}
-	if sizeSummaryFloats < floatL2 {
-		timesAlgorithmParallelBuf1(a, b, c, m, n, h)
+
+	fmt.Println("threads = ", threads)
+
+	memPerTh := memory / float64(threads)
+
+	fmt.Printf("memory = %8v\n", memPerTh)
+
+	// parallel algo
+	alpha := (memPerTh - float64(n)) / (float64(n) + 1.)
+	if alpha > float64(m) {
+		alpha = float64(m)
+	}
+	betta := (memPerTh - 2.*alpha) / (float64(n) + 2.*alpha)
+	if betta > float64(h) {
+		betta = float64(h)
+	}
+	// amount rows of [A]
+	iAlpha := int(alpha)
+	// amount columns of [B]
+	iBetta := int(betta)
+
+	fmt.Println("<", iAlpha, ",", iBetta, ">")
+
+	// Create workgroup
+	if alpha > betta {
+
+		fmt.Println("parallel strategy")
+
+		var wg sync.WaitGroup
+		// Run calculation in goroutines
+		step := int(float64(h) / float64(threads))
+		from := 0
+		var to int
+		for t := 0; t < threads; t++ {
+			to = from + step
+			if to > h {
+				to = h
+			}
+			// Add one goroutine in workgroup
+			wg.Add(1)
+			go func(from, to int) {
+				// Change waitgroup after work done
+				defer wg.Done()
+				var sum float64
+				buf := make([]float64, n, n)
+				for j := from; j < to; j++ {
+					for k := 0; k < n; k++ {
+						buf[k] = (*b)[k][j]
+					}
+					for i := 0; i < m; i++ {
+						sum = 0
+						for k := 0; k < n; k++ {
+							sum += (*a)[i][k] * buf[k]
+						}
+						(*c)[i][j] = sum
+					}
+				}
+			}(from, to)
+			from = to
+		}
+		wg.Wait()
 		return
 	}
-	// parallel algo
-	//if sizeSummaryFloats < floatL3 {
-	//}
-	//alpha := (memory - float64(n)) / (float64(n) + 1.)
-	//if alpha > float64(m) {
-	//	alpha = float64(m)
-	//}
-	//betta := (memory - 2.*alpha) / (float64(n) + 2.*alpha)
-	// amount rows of [A]
-	//iAlpha := int(alpha)
-	// amount columns of [B]
-	//iBetta := int(betta)
-	for i := 0; i < m; i++ {
-		for j := 0; j < h; j++ {
-			for k := 0; k < n; k++ {
-				(*c)[i][j] += (*a)[i][k] * (*b)[k][j]
-			}
-		}
-	}
-}
 
-func timesAlgorithmSimple(a, b, c *[][]float64, m, n, h int) {
+	fmt.Println("base strategy")
+
+	// add strategy for many buffers
 	var sum float64
 	for j := 0; j < h; j++ {
 		for i := 0; i < m; i++ {
@@ -63,41 +112,4 @@ func timesAlgorithmSimple(a, b, c *[][]float64, m, n, h int) {
 			(*c)[i][j] = sum
 		}
 	}
-}
-
-func timesAlgorithmParallelBuf1(a, b, c *[][]float64, m, n, h int) {
-	// Create workgroup
-	var wg sync.WaitGroup
-	// Run calculation in goroutines
-	step := int(float64(h) / float64(threads))
-	from := 0
-	var to int
-	for t := 0; t < threads; t++ {
-		to = from + step
-		if to > h {
-			to = h
-		}
-		// Add one goroutine in workgroup
-		wg.Add(1)
-		go func(from, to int) {
-			// Change waitgroup after work done
-			defer wg.Done()
-			var sum float64
-			buf := make([]float64, n, n)
-			for j := from; j < to; j++ {
-				for k := 0; k < n; k++ {
-					buf[k] = (*b)[k][j]
-				}
-				for i := 0; i < m; i++ {
-					sum = 0
-					for k := 0; k < n; k++ {
-						sum += (*a)[i][k] * buf[k]
-					}
-					(*c)[i][j] = sum
-				}
-			}
-		}(from, to)
-		from = to
-	}
-	wg.Wait()
 }
--- matrix/times64_test.go
+++ matrix/times64_test.go
@@ -35,7 +35,6 @@ func BenchmarkTimes64(b *testing.B) {
 		{2000, 1, 2000},
 		{2000, 2000, 2000},
 	}
-
 	for _, bm := range benchmarks {
 		b.Run(fmt.Sprintf("%5v%5v%5v", bm.m, bm.n, bm.h), func(b *testing.B) {
 			b.StopTimer()
@@ -59,6 +58,57 @@ func BenchmarkTimes64(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				timesAlgorithm(&A, &B, &C, bm.m, bm.n, bm.h)
 			}
+		})
+	}
+}
+
+func TestTimes(t *testing.T) {
+	benchmarks := []struct {
+		m, n, h int
+	}{
+		{1, 1, 1},
+
+		{5, 1, 1},
+		{1, 5, 1},
+		{1, 1, 5},
+		{5, 5, 1},
+		{1, 5, 5},
+		{5, 1, 5},
+		{5, 5, 5},
+
+		{1000, 3, 3},
+		{3, 1000, 3},
+		{3, 3, 1000},
+		{1000, 1000, 3},
+		{3, 1000, 1000},
+		{1000, 3, 1000},
+		{1000, 1000, 1000},
+
+		{2000, 3, 3},
+		{3, 2000, 3},
+		{3, 3, 2000},
+		{2000, 2000, 3},
+		{3, 2000, 2000},
+		{2000, 3, 2000},
+		{2000, 2000, 2000},
+	}
+	for _, bm := range benchmarks {
+		t.Run(fmt.Sprintf("%5v%5v%5v", bm.m, bm.n, bm.h), func(t *testing.T) {
+			A := make([][]float64, bm.m, bm.m)
+			for r := 0; r < bm.m; r++ {
+				A[r] = make([]float64, bm.n, bm.n)
+			}
+
+			B := make([][]float64, bm.n, bm.n)
+			for r := 0; r < bm.n; r++ {
+				B[r] = make([]float64, bm.h, bm.h)
+			}
+
+			C := make([][]float64, bm.m, bm.m)
+			for r := 0; r < bm.m; r++ {
+				C[r] = make([]float64, bm.h, bm.h)
+			}
+			timesAlgorithm(&A, &B, &C, bm.m, bm.n, bm.h)
 		})
 	}
 }
