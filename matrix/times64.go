0ff055c59615bad127fbdfd5153b80b56e5978bc
Konstantin8105 <konstantin8105@gmail.com>
2017-07-19 17:43:29 +0300

    pre

--- matrix/times64.go
+++ matrix/times64.go
@@ -89,7 +89,7 @@ func timesAlgorithm(a, b, c *[][]float64, m, n, h int) {
 		return
 	}
 
-	// add strategy for many buffers
+	//if m > n && h > n {
 	var sum float64
 	for j := 0; j < h; j++ {
 		for i := 0; i < m; i++ {
@@ -100,4 +100,64 @@ func timesAlgorithm(a, b, c *[][]float64, m, n, h int) {
 			(*c)[i][j] = sum
 		}
 	}
+	return
+	//	}
+	/*
+		// add strategy for many buffers
+		var wg sync.WaitGroup
+		// Run calculation in goroutines
+		step := int(float64(h) / float64(threads))
+		from := 0
+		var to int
+		for t := 0; t < threads; t++ {
+			if from >= h {
+				continue
+			}
+			to = from + step
+			if to > h {
+				to = h
+			}
+			// Add one goroutine in workgroup
+			wg.Add(1)
+			go func(from, to int) {
+				// Change waitgroup after work done
+				defer wg.Done()
+				var sum0 float64
+				var sum1 float64
+				buf0 := make([]float64, n, n)
+				buf1 := make([]float64, n, n)
+				for j := from; j < to; j += 2 {
+					for k := 0; k < n; k++ {
+						buf0[k] = (*b)[k][j+0]
+						buf1[k] = (*b)[k][j+1]
+					}
+					for i := 0; i < m; i++ {
+						sum0 = 0
+						sum1 = 0
+						for k := 0; k < n; k++ {
+							sum0 += (*a)[i][k] * buf0[k]
+							sum1 += (*a)[i][k] * buf1[k]
+						}
+						(*c)[i][j+0] = sum0
+						(*c)[i][j+1] = sum1
+					}
+				}
+				if (to-from)/2 != int(float64(to-from)/2.) {
+					j := to - 1
+					for k := 0; k < n; k++ {
+						buf0[k] = (*b)[k][j+0]
+					}
+					for i := 0; i < m; i++ {
+						sum0 = 0
+						for k := 0; k < n; k++ {
+							sum0 += (*a)[i][k] * buf0[k]
+						}
+						(*c)[i][j+0] = sum0
+					}
+				}
+			}(from, to)
+			from = to
+		}
+		wg.Wait()
+	*/
 }
